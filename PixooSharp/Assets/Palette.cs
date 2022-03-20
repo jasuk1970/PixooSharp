7d8efb9f7d98ee8e8454c931474782b75fae630f
Jason Dean <mail@jsun.me.uk>
2022-03-20 09:01:06 +0000

    Add grey and yellow palette colours

--- PixooSharp/Assets/Palette.cs
+++ PixooSharp/Assets/Palette.cs
@@ -4,8 +4,10 @@
     {
         public static Rgb Black { get; set; } = new Rgb() { Red = 0, Green = 0, Blue = 0};
         public static Rgb White { get; set; } = new Rgb() { Red = 255, Green = 255, Blue = 255};
+        public static Rgb Grey { get; set; } = new Rgb() { Red = 180, Green = 180, Blue = 180};
         public static Rgb Red { get; set; } = new Rgb() { Red = 255, Green = 0, Blue = 0 };
         public static Rgb Green { get; set; } = new Rgb() { Green = 255, Red = 0, Blue = 0 };
+        public static Rgb Yellow { get; set; } = new Rgb() { Red = 255, Green= 255, Blue = 0 };
       
     }
 }
